2bca261f1d9cd8b78fd283a7aa8d7f31c22530bb
nati <nati@cooladata.com>
2018-05-14 11:23:09 +0300

    Add

--- app/constants/apis.js
+++ app/constants/apis.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGO_URL: 'mongodb://localhost:27017/bookout',
+    MONGO_URL: 'mongodb://natylok:adsasf54654dfasA@ds219130.mlab.com:19130/bookout',
     FACEBOOK_AUTH_URL:'https://graph.facebook.com/me?fields=id,name,email,picture.type(large)&access_token=<TOKEN>'
 }
