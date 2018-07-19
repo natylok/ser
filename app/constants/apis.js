4aa05153ec300bbd3219580aff88a6790f702d51
giteran <eran3326@gmail.com>
2018-07-19 18:53:11 +0300

    TestingPush

--- app/constants/apis.js
+++ app/constants/apis.js
@@ -4,3 +4,5 @@ module.exports = {
     MONGO_URL: 'mongodb://localhost:27017/bookout',
     FACEBOOK_AUTH_URL:'https://graph.facebook.com/me?fields=id,name,email,picture.type(large)&access_token=<TOKEN>'
 }
+
+//walak test le branch
